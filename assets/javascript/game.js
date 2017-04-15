9f2d847d79d8076bbeb4ea10adcd0a6d4309b0b6
Cullan Shewfelt <cullanshewfelt@github.com>
2017-04-14 18:53:27 -0700

    working! no sound

---
+++ assets/javascript/game.js
@@ -0,0 +1,72 @@
+var randomValue;
+var randomValuesArray = []
+var counter = 0;
+var targetScore;
+var wins = 0;
+var losses = 0;
+
+
+function ready(gameStart) {
+    randomNumber();
+    randomValue();
+    $("#your-score").text(counter);
+    $("#wins").text(wins);
+    $("#losses").text(losses)
+}
+
+function randomNumber() {
+    targetScore = Math.floor(Math.random() * (120 - 19 + 1) + 19);
+    $("#target-score").text(targetScore);
+}
+
+function randomValue() {
+    for (i = 0; i < 4; i++) {
+        var randomValue = Math.floor(Math.random() * (12) + 1);
+        randomValuesArray.push(randomValue);
+    }
+    for (i = 0; i < randomValuesArray.length; i++) {
+        $("#gem" + i).attr("data-gemvalue", randomValuesArray[i]);
+    }
+}
+
+$(".gems").on("click", function() {
+    var gemValue = ($(this).attr("data-gemvalue"));
+    gemValue = parseInt(gemValue);
+    $("#your-score").text(counter += gemValue);
+
+    if (counter === targetScore) {
+        alert("You Win!");
+        wins++;
+        counter = 0;
+        randomNumber();
+        randomValuesArray=[];
+        randomValue();
+        // $("#target-score").text(targetScore);
+        $("#your-score").text(counter);
+        $("#wins").text(wins);
+    }
+    else if (counter >= targetScore) {
+        alert("You Lose!");
+        losses++;
+        counter = 0;
+        randomNumber();
+        randomValuesArray=[];
+        randomValue();
+        $("#your-score").text(counter);
+        $("#losses").text(losses);
+    }
+})
+
+ready()
+
+
+
+// randomValuesArray.push(randomValue())
+
+
+// randomValue()
+
+// console.log(randomValue())
+
+// console.log(randomValue)
+// console.log(targetScore)
